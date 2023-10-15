29ee84786c50c9d470d31ea2bef5fa8f5c7462ab
Nugar <cladeadenugar@gmail.com>
2023-10-15 14:09:51 +0100

    added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const appRoute = require("./routes/route.js");
 
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 /*routes */
 app.use("/", appRoute);
